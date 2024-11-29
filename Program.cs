4290ce2c51c238d8384e4500c83439206b5f95bb
whitemousetl <2577167951@qq.com>
2024-11-29 17:23:37 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ValueReferencePass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PassByValue();
+            PassByReference();
+        }
+
+        //
+        private static void PassByValue()
+        {
+            static void ModifyValue(int a)
+            {
+                a = 10;
+                Console.WriteLine(a);
+            };
+
+            int x = 5;
+            ModifyValue(x);
+            Console.WriteLine(x);
+        }
+
+        //
+        private static void PassByReference()
+        {
+
+        }
+    }
+}
